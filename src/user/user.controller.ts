f00aa0af71b287382395653f478815bcdfc3872a
joceesagar <joshisagarm2@gmail.com>
2025-11-26 20:04:43 +0545

    implemented custom guards route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import type { Request } from 'express';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
 
+@UseGuards(JwtGuard) //--> root level guard
 @Controller('users')
 export class UserController {
     // @UseGuards(AuthGuard('jwt')) --> without importing from seperate folder
-    @UseGuards(JwtGuard)
+    // @UseGuards(JwtGuard) --> Route level guard
     @Get('me')
     getMe(@Req() req: Request) {
         return req.user;
